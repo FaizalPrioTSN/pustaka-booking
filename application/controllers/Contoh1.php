1fed58cfc6b99e528c28bdfaff19ae925a3e9968
faizalpriotsn <faizalprio.yk1@gmail.com>
2023-09-25 16:31:27 +0700

    tambah data

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalan</h1>";
+        echo"Nama Saya Faizal Prio Tri Setyo Nugroho
+        Saya Tinggal di daerah Kulon Progo
+        olah raga yang saya sukai adalah
+        Bulu tangkis";
+    }
+}
